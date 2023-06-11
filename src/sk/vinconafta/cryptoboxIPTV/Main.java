12c1039ed03efa6d633cd4787f8e9c92d0920865
VincoNafta <62835475+VincoNafta@users.noreply.github.com>
2023-06-11 10:36:40 +0200

    Update Main.java

--- src/sk/vinconafta/cryptoboxIPTV/Main.java
+++ src/sk/vinconafta/cryptoboxIPTV/Main.java
@@ -2,8 +2,6 @@ package sk.vinconafta.cryptoboxIPTV;
 
 import sk.vinconafta.cryptoboxIPTV.gui.Menu;
 
-import javax.swing.JOptionPane;
-
 public class Main {
     public static void main(String[] args) {
         new Menu();
