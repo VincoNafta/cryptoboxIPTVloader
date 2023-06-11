cf3365ea10ef975b7b9f58ed7e0918d2d61940b7
VincoNafta <62835475+VincoNafta@users.noreply.github.com>
2023-06-11 10:36:26 +0200

    Update Main.java

--- src/sk/vinconafta/cryptoboxIPTV/Main.java
+++ src/sk/vinconafta/cryptoboxIPTV/Main.java
@@ -7,7 +7,6 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         new Menu();
-//        JOptionPane.showMessageDialog(null,"Progam používate na vlastnú zodpovednosť.\n Autor programu neručí za škody", "UPOZORNENIE!!!" , JOptionPane.WARNING_MESSAGE);
 
     }
 }
